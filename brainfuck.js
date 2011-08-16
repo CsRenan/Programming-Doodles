93b2f9b30e5c4af408d99e6a9419e4be3b31007c
Renan C. Sousa <csrenan@csrenan.com>
2011-08-15 22:41:36 -0300

    adding Brainfuck

---
+++ brainfuck.js
@@ -0,0 +1,94 @@
+// Brainfuck is the most minimalistic scripting language ever.
+// Some years ago, I made an interpreter using C#. Now I'm porting it to Javascript.
+// For the language specification, refer to http://esolangs.org/wiki/Brainfuck
+// Maybe I'll add comments to the code below someday.
+// For now, rest assured that I have tested it against some examples, from hello world and cat programs to the more complex calculators.
+
+function brainfuck(code) {
+
+  var data = [];
+  data.length = 30000;
+
+  var j;
+  for (j = 0; j < 30000; j++) {
+    data[j] = 0;
+  }
+  j = 0;
+
+  var printQueue = "";
+
+  for (var i = 0; i < code.length; i++) {
+    if (code[i] == ">") {
+      j++;
+      if (j >= 30000) {
+        throw new Error("Data pointer overflow.");
+      }
+    } else if (code[i] == "<") {
+      j--;
+      if (j < 0) {
+        throw new Error("Data pointer underflow");
+      }
+    } else if (code[i] == "+") {
+      data[j]++;
+      if (data[j] > 255) {
+        data[j] = 0;
+      }
+    } else if (code[i] == "-") {
+      data[j]--;
+      if (data[j] < 0) {
+        data[j] = 255;
+      }
+    } else if (code[i] == ".") {
+      printQueue += String.fromCharCode(data[j]);
+    } else if (code[i] == ",") {
+      var input = prompt("Input a single character at position " + j + " (extra characters will be ignored).");
+      data[j] = input.charCodeAt(0);
+    } else if (code[i] == "[") {
+      if (data[j] == 0) {
+        var bracketCount = 0;
+        for (var k = i + 1; k < code.length; k++) {
+          if (code[k] == "[") {
+            bracketCount++;
+          } else if (code[k] == "]") {
+            bracketCount--;
+          }
+
+          if (bracketCount < 0) {
+            break;
+          }
+        }
+
+        if (k == code.length) {
+          throw new Error("Unmatched bracket at index " + i + ".")
+        } else {
+          i = k;
+        }
+      }
+    } else if (code[i] == "]") {
+      if (data[j] != 0) {
+        var bracketCount = 0;
+        for (var k = i - 1; k > 0; k--) {
+          if (code[k] == "]") {
+            bracketCount++;
+          } else if (code[k] == "[") {
+            bracketCount--;
+          }
+
+          if (bracketCount < 0) {
+            break;
+          }
+        }
+
+        if (k == 0) {
+          throw new Error("Unmatched bracket at index " + i + ".")
+        } else {
+          i = k;
+        }
+      }
+    }
+  }
+
+  if (printQueue.length > 0) {
+    return printQueue;
+  }
+}
